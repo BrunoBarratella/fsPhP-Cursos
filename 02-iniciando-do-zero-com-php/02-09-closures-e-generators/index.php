b9a330bcb5db1d3e90ff28d8f5b859ab06cdaa16
Bruno <brubarratella@hotmail.com>
2023-03-13 14:54:21 -0300

    Conteudo sobre closures (Funções Anônimas)

--- 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
+++ 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
@@ -7,6 +7,31 @@ fullStackPHPClassName("02.09 - Closures e generators");
  */
 fullStackPHPClassSession("closures", __LINE__);
 
+$myAge = function($year){
+    $age = date("Y") - $year;
+    return "<p>Você tem {$age} anos!</p>";
+};
+
+echo $myAge(2004);
+
+$priceBrl = function ($price){
+    return number_format($price, 2, ",", ".");
+};
+
+echo "<p>O projeto custa R$ {$priceBrl(3600)}. Vamos fechar?</p>";
+
+$myCart =[];
+$myCart["totalPrice"] = 0;
+$cartAdd = function ($item, $qtd, $price) use (&$myCart){
+    $myCart[$item] = $qtd * $price;
+    $myCart["totalPrice"] += $myCart[$item];
+};
+
+$cartAdd("HTML5", 1, 497);
+$cartAdd("JQuery", 2, 500);
+
+var_dump($myCart);
+
 
 /*
  * [ generators ] https://php.net/manual/pt_BR/language.generators.overview.php
